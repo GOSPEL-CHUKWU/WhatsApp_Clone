3cac71c5b8b839417c3c3e92ab8bc5dd32cba66d
gospel-chukwu <gospelchukwu.me@gmail.com>
2023-06-22 20:54:50 +0100

    'chatlistItems'

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,22 +1,22 @@
 import { Text, View, Image, StyleSheet } from 'react-native';
 
-const index = () => {
+const index = ({chat}) => {
   return (
     <View style={styles.container}>
       <Image
         source={{
-          uri: 'http://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
+          uri: {chat.user.image},
         }}
         style={styles.image}
       />
 
-      <View style={styles.content}>
+      <View style={styles.conternt}>
         <View style={styles.row}>
-          <Text numberOfLines={1}  style={styles.name}>Lukas Ben </Text>
-          <Text style={styles.subTitle}>8:45</Text>
+          <Text numberOfLines={1}  style={styles.name}>{chat.user.name}</Text>
+          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
         </View>
 
-        <Text numberOfLines={1} style={styles.subTitle}>Hello Lukas</Text>
+        <Text numberOfLines={1} style={styles.subTitle}>{chat.lastMessage.text}</Text>
       </View>
     </View>
   );
